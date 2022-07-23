a19f2fc8fe2a687160389dc6413ce37c4dd674f0
Juanma <juanmaper30@gmail.com>
2022-07-23 12:46:58 +0200

    Add map module to vuex store

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,16 +4,21 @@ import { createStore } from 'vuex';
 import placesModule from './places';
 import { PlacesState } from './places/state';
 
+import { MapState } from './map/state';
+import mapModule from './map/index';
+
 
 export interface StateInterface {
   // Define your own store structure, using submodules if needed
   // example: ExampleStateInterface;
   // Declared as unknown to avoid linting issue. Best to strongly type as per the line above.
-  places: PlacesState
+  places: PlacesState,
+  map: MapState
 }
 
 export default createStore<StateInterface>({
   modules: {
-    places: placesModule
+    places: placesModule,
+    map: mapModule
   }
 })
