02023e6947e5634a2004ee34c9f75a6b7e3f9cff
Juanma <juanmaper30@gmail.com>
2022-07-24 13:56:52 +0200

    Create api file to requesst locations

---
+++ src/apis/searchApi.ts
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+
+const searchApi = axios.create({
+    baseURL: 'https://api.mapbox.com/geocoding/v5/mapbox.places',
+    params: {
+        limit: 5,
+        language: 'es',
+        access_token: 'pk.eyJ1IjoianVhbm1hcGVyIiwiYSI6ImNsNXYzaG9hZjAxbTgzZGxjNjYwNXM1MTIifQ.-oX65tNNjHYyvjdvuftSbg'
+    }
+});
+
+
+export default searchApi;
